4939d8f28154c87e05cefdb683a675f8e581fd63
George Doenlen <gdoenlen@gmail.com>
2018-12-10 16:23:08 -0500

    refactorings

--- src/components/auth.js
+++ src/components/auth.js
@@ -7,58 +7,117 @@ import {
   DataTableColumn,
   DataTableRowActions,
   Dropdown,
-  PageHeader
+  PageHeader,
+  Modal
 } from '@salesforce/design-system-react';
+import sfdx from '../services/sfdx';
 
 /**
  * Authorization panel component
  */
 export default class Auth extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      auths: [],
+      form: {
+
+      }
+    };
+
+    this.aliasChange = this.aliasChange.bind(this);
+  }
+
+  async componentDidMount() {
+    try {
+      const result = await sfdx.getOrgs();
+      result.result.nonScratchOrgs.forEach((org, i) => org.id = i.toString());
+      this.setState({
+        auths: result.result.nonScratchOrgs
+      });
+    } catch (err) {
+      console.log('error caught');
+    }
+  }
+
+  /**
+   * Handles the state change from the alias field on the new form.
+   * @param {event} event 
+   */
+  aliasChange(event) {
+    this.setState(state => {
+      state.form.alias = event.currentTarget.value;
+      return state;
+    });
+  }
+
+  /**
+   * Saves a new authorization
+   * @param {string} alias - the alias you want the new authorization set to
+   */
+  async saveAuth(alias) {
+    try {
+      const result = await sfdx.newAuth(alias);
+    } catch (err) {
+      //todo
+    }
+  }
+
   render() {
     const navRight = (
       <div>
         <ButtonGroup>
-          <Button variant="brand" label="New"/>
+          <Button variant="brand" label="New" onClick={() => this.setState({ modalOpen: true })}/>
         </ButtonGroup>
+        <Modal 
+          isOpen={this.state.modalOpen} 
+          title="New Auth" 
+          onRequestClose={() => this.setState({ modalOpen: false })}
+          footer={[
+            <Button label="Cancel" onClick={() => this.setState({ modalOpen: false})}/>,
+            <Button label="Save" onClick={() => this.saveAuth(this.state.form.alias)}/>
+          ]}  
+        >
+          <section className="slds-p-around_large">
+            <div className="slds-form-element slds-m-bottom_large">
+              <label className="slds-form-element_label" htmlFor="alias">
+                Alias
+              </label>
+              <div className="slds-form-element_control">
+                <input id="alias" className="-input" type="text" onChange={this.aliasChange}/>
+              </div>
+            </div>
+          </section>
+        </Modal>
       </div>
     );
+    
     return (
       <React.Fragment>
         <Header/>
-        <PageHeader label="Auths" title="Auths" info="# of items" variant="objectHome" navRight={navRight}/>
+        <PageHeader label="Orgs" title="Auths" info={this.state.auths.length.toString()} variant="objectHome" navRight={navRight}/>
         <div>
-          <DataTable items={[
-            {
-              id: '1',
-              alias: 'gd',
-              username: 'gd@example.com',
-              orgId: '1234',
-              connectedStatus: 'connected'
-            }
-          ]}>
+          <DataTable items={this.state.auths}>
             <DataTableColumn label="Alias" property="alias"/>
             <DataTableColumn label="Username" property="username"/>
             <DataTableColumn label="Org Id" property="orgId"/>
             <DataTableColumn label="Connected Status" property="connectedStatus"/>
             <DataTableRowActions options={[
                 {
-                  id: 0,
-                  label: 'Edit',
-                  value: '1'
-                },
-                {
-                  id: 1, 
+                  id: 0, 
                   label: 'Delete',
-                  value: '2'
+                  value: 'delete'
                 },
                 {
-                  id: 2,
+                  id: 1,
                   label: 'Set Alias',
-                  value: '3'
+                  value: 'alias'
                 }
               ]}
               dropdown={<Dropdown length="5" iconCategory="utility" iconName="down"/>}
+              onAction={(item, action) => console.log(item, action)}
             />
           </DataTable>
         </div>
---
+++ src/services/sfdx.js
@@ -0,0 +1,60 @@
+//this only works when running from electron!!
+const exec = window.require('child_process').exec;
+
+/**
+ * Service to interact with the native sfdx binary.
+ */
+class SfdxService {
+  
+  /**
+   * Creates a new service
+   * @constructor 
+   * @param {function} exec - executes a function against a system binary 
+   */
+  constructor(exec) {
+    this.exec = exec;
+  }
+
+  /**
+   * Creates a new authorization
+   * @param {string} alias - the alias to set for this auth
+   * @returns {promise}
+   */
+  async newAuth(alias) {
+    let cmd = 'sfdx force:auth:web:login --json';
+    if (alias) {
+      cmd += ` --setalias ${alias}`;
+    }
+    
+    return this._exec(cmd);
+  }
+
+  /**
+   * Gets all org listings
+   * { status: 0, result: { nonScratchOrgs: [], scratchOrgs: []} }
+   * @returns {promise}
+   */
+  async getOrgs() {
+    return this._exec('sfdx force:org:list --json');
+  }
+  
+  /**
+   * Executes a shell command 
+   * @param {string} cmd - shell command to execute
+   * @returns {promise}
+   */
+  async _exec(cmd) {
+    return new Promise((resolve, reject) => {
+      this.exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(stdout));
+        }
+      });
+    });
+  }
+}
+
+const sfdx = new SfdxService(exec);
+export default sfdx;
