8f019351b47d557d6c61a784e1cf69529db3fd05
George Doenlen <gdoenlen@gmail.com>
2018-12-12 12:17:53 -0500

    add docs

--- src/components/routableglobalnavigationbarlink.js
+++ src/components/routableglobalnavigationbarlink.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+/**
+ * Navigation bar link meant to mimic the GlobalNavigationBarLink from the react design system.
+ * This implementation allows use of react router.
+ */
 export default class RoutableGlobalNavigationBarLink extends Component {
   render() {
     return (
       <li className="slds-context-bar__item">
         <NavLink to={this.props.href} className="slds-context-bar__label-action" activeClassName="slds-is-active">
-          {this.props.label}
+          <span className="slds-truncate">{this.props.label}</span>
         </NavLink>
       </li>
     );
